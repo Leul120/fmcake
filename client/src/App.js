0e2eb1e5a55ce89e77fe6da1633c238156eb147d
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-09-11 12:03:19 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import ShopDashboard from './ShopDashboard';
 import ReceptionistDashboard from './ReceprionistDashboard';
 import BakerDashboard from './BakerDashboard';
 import socketIO from 'socket.io-client';
-const socket = socketIO.connect('https://customcake4.onrender.com');
+const socket = socketIO.connect('http://customcake4.vercel.app');
 export const AppContext=createContext()
 // const socket=0
 function App() {
